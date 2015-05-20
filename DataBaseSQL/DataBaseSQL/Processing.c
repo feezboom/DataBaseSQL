b269984377c7154f0ecf1ac3d042eee58036e930
Andrew <algebroid.2011@gmail.com>
2015-05-20 17:51:41 +0300

    Almost ready

--- DataBaseSQL/DataBaseSQL/Processing.c
+++ DataBaseSQL/DataBaseSQL/Processing.c
@@ -1,21 +1,13 @@
 #include "Database.h"
-
-#include <fcntl.h>
 #include <string.h>
+#include <stdio.h>
 
 //Написать программу для обработки пользовательских запросов
-//На русском языке
-bool opened(char* BaseName)
-{
-	return !strcmp("", BaseName) ? false : true;
-}
 
-
-
-int PerformQueries(sqlite3 *MyBase, char* Queries)
+int PerformQueries(sqlite3 *MyBase, char* Queries, int length)
 {
 	const char *pos = Queries;										//Начало нераспарсенной части запросов
-	const char *end = Queries + strlen(Queries);					//Конец всех запросов
+	const char *end = Queries + length;								//Конец всех запросов
 	
 	int rc; sqlite3_stmt *st;
 
@@ -27,14 +19,14 @@ int PerformQueries(sqlite3 *MyBase, char* Queries)
 		{
 			int	i;
 			for (i = 0; i < sqlite3_column_count(st); ++i)
-				printf("%s", sqlite3_column_text(st, i));
+				printf(i == 0 ? "%s|" : "%30s|", sqlite3_column_text(st, i));
 			printf("\n");
 		}
 
-		if (rc != SQLITE_DONE && rc != SQLITE_ROW)					//Если вдруг возникла какая-то ошибка, то вывести её
+		if (rc != SQLITE_DONE && rc != SQLITE_ROW && rc != SQLITE_OK)//Если вдруг возникла какая-то ошибка, то вывести её
 		{
-			printf("Error : %s\n", sqlite3_errmsg(MyBase));
-			system("pause");
+			printf("Error %d : %s\n", rc, sqlite3_errmsg(MyBase));
+			getchar();
 			return -1;
 		}
 
@@ -42,64 +34,63 @@ int PerformQueries(sqlite3 *MyBase, char* Queries)
 	}
 	return 0;
 }
+bool CheckQuery(char* Query)
+{
+	if (strstr(Query, "'"))
+	{
+		printf("Wrong query.\n");
+		return 0;
+	}
+	else 
+		return 1;
+}
+
+
 int Processing()
 {
 	char Queries[MAX_STRING_SIZE];
 	char Command[MAX_STRING_SIZE];
-	char BaseName[20] = "";
+	char BaseName[20] = "Restaraunt.db3";
 
-	printf("Type \"exit\" if you want to finish program.\n");
-	FILE * in; sqlite3 *MyBase;
-	bool OK = true;
-	while (strcmp(Command, "exit"))
+	sqlite3 *MyBase;
+	int rc = sqlite3_open(BaseName, &MyBase);
+	if (rc)
+	{
+		fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(MyBase));
+		sqlite3_close(MyBase);
+		BaseName[0] = '\0';
+	}
+
+	printf("Here you can type your commands:\n");
+	printf(" - 'exit' to close program\n");
+	printf(" - 'clear' to clear screen\n");
+	printf(" - 'sql [sql query]' to perform sql query\n");
+	printf(" - 'script [scriptname]' to perform script\n");
+
+	while (true)
 	{
 		printf("Command: ");
 		scanf("%s", Command);
 
-		if (!strcmp("open", Command))
-		{
-			if (opened(BaseName)) sqlite3_close(MyBase);
-			scanf("%s", BaseName);
-			strcat(BaseName, ".sqlite3");
-			int rc = sqlite3_open(BaseName, &MyBase);
-			if (rc)
-			{
-				fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(MyBase));
-				sqlite3_close(MyBase);
-				BaseName[0] = '\0';
-			}
-			continue;
-		}
+		if (!strcmp("exit", Command))
+			exit(0);
 
-		if (!strcmp("perform", Command))
+		if (!strcmp("script", Command))
 		{
-			if (!opened(BaseName))
+			char filename[1024] = "";
+			scanf("%s", filename);
+
+			FILE* in = fopen(filename, "r");
+			if (in == NULL)
 			{
-				printf("DataBase %s wasn't opened...", BaseName);
+				printf("Error opening script %s.\n", filename);
 				continue;
 			}
-			char type[10];
-			scanf("%s", type);
-			if (!strcmp("sql", type))
-			{
-				scanf("%s", Queries);
-				PerformQueries(MyBase, Queries);
-			}
-			if (!strcmp("file", type))
-			{
-				char filename[40] = "";
-				scanf("%s", filename);
-				FILE* in = fopen(filename, "r");
-				if (in == NULL)
-				{
-					printf("Error opening file %s", filename);
-					continue;
-				}
-				char c; int i = 0;
-				while ((c = fgetc(in)) != EOF) Queries[i++] = c;
-				Queries[i] = '\0';
-				PerformQueries(MyBase, Queries);
-			}
+			char c; int i = 0;
+			while ((c = fgetc(in)) != EOF) Queries[i++] = c;
+			Queries[i] = '\0';
+
+			PerformQueries(MyBase, Queries, i);
 			continue;
 		}
 		if (!strcmp("sql", Command))
@@ -107,63 +98,17 @@ int Processing()
 			char t; int i = 0;
 			while ((t = getchar()) != '\n') Queries[i++] = t;
 			Queries[i] = '\0';
-			PerformQueries(MyBase, Queries);
+			if (!CheckQuery(Queries))
+				continue;
+			PerformQueries(MyBase, Queries, i);
 			continue;
 		}
-		if (!strcmp("close", Command))
+		if (!strcmp("clear", Command))
 		{
-			if (opened(BaseName))
-			{
-				sqlite3_close(MyBase);
-				BaseName[0] = "\0";
-			}
+			printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
 			continue;
 		}
-		if (!strcmp("insert", Command))
-		{
-			FILE* insert = fopen("insert.txt", "r");
-			char TableName[MAX_STRING_SIZE];
-			printf("TableName --> ");
-			scanf("%s", TableName);
-			printf("To finish entering params print '_ok'\n");
-			printf("Params --> "); 
-			char str[MAX_STRING_SIZE]; 
-			char param[MAX_STRING_SIZE];
-			scanf("%s", param);
-			sprintf(str, "INSERT INTO %s VALUES(", TableName);
-			if (strcmp("_ok", param))
-			{
-				strcat(str, "'");
-				strcat(str, param);
-				strcat(str, "'");
-			}
-			scanf("%s", param);
-			while (strcmp("_ok", param))
-			{
-				strcat(str, ", '");
-				strcat(str, param);
-				strcat(str, "'");
-				scanf("%s", param);
-			}
-			strcat(str, ");");
-			PerformQueries(BaseName, str);
-			
-
-			continue;
-		}
-		if (!strcmp("create", Command))
-		{
-		}
-		if (!strcmp("create", Command))
-		{
-		}
-		if (!strcmp("create", Command))
-		{
-		}
-
 		printf("Unknown command: %s\n", Command);
 	}
-
-
 	return 0;
 }
